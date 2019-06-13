6d84cbe640cb600124e41d8c3de7b1d3c4565484
bujosa <davidbujosa@gmail.com>
2019-06-12 23:24:59 -0400

    Enrique

---
+++ CodeKatas/ArgsClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeKatas
+{
+    public class InvalidSchemeException : Exception { };
+    public class ArgsClass
+    {   
+        
+
+        public Dictionary<string, bool> Args(string[] scheme, string args)
+        {
+            var dict = new Dictionary<string, bool>();
+
+            if (scheme.Length == 0)
+                throw new Exception("No scheme");     
+
+            foreach (string command in scheme)
+            {
+                if (command.Length != 2 && command[0] != '-')
+                    throw new InvalidSchemeException();
+                else
+                {
+                    dict.Add(command, false);
+
+                }
+            }
+            return dict;
+        }
+    }
+}
---
+++ NTest/Program.cs
@@ -0,0 +1,65 @@
+﻿using CodeKatas;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace NTest
+{
+    [TestFixture]
+    class Program
+    {
+        [Test]
+        public void EmtyString()
+        {
+            var scheme = new string[0];
+            var args = "";
+            var control = new ArgsClass();
+            var expected = new Exception("No scheme");
+            var actual = control.Args(scheme, args);
+
+        
+        }
+        [Test]
+        public void OneScheme()
+        {
+            var scheme = new string[1];
+            scheme[0] = "-p";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+
+            var actual = control.Args(scheme, null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TwoScheme()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-p";
+            scheme[1] = "-x";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+            expected.Add(scheme[1], false);
+
+            var actual = control.Args(scheme, null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void InvalidScheme()
+        {
+            var scheme = new string[2];
+            scheme[0] = "-px";
+            scheme[1] = "-x";
+            var control = new ArgsClass();
+            var expected = new Dictionary<string, bool>();
+            expected.Add(scheme[0], false);
+            expected.Add(scheme[1], false);
+
+            Assert.Throws<InvalidSchemeException>(() => control.Args(scheme, null));
+        }
+
+    }
+}
